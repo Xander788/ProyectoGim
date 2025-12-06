375c3c90279f231e6b2784393f4e92cb0fe1b4b8
Jhadri <joadrojasro@est.utn.ac.cr>
2025-12-05 18:09:50 -0600

    Se creo las clases Imapper y clase mapper
    ademas se modifico pago, PagoDTO y ClaseDTO

---
+++ src/Mappers/ClaseMapper.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mappers;
+
+import DTO.ClaseDTO;
+import Modelo.Clase;
+
+//   private int id;
+//    private String tipo;
+//    private String horario;
+//    private int capacidadMaxima;
+//    private int idEntrenador;
+
+/**
+ *
+ * @author Jorge
+ */
+public class ClaseMapper implements IMapper<Clase, ClaseDTO>{
+
+
+    
+    @Override
+    public ClaseDTO ToDto(Clase ent) {
+       return new ClaseDTO(ent.getId(),ent.getTipo(), ent.getHorario(), ent.getCapacidadMaxima());
+    }
+
+    @Override
+    public Clase ToEntidad(ClaseDTO dto, Entrenador id) {
+        return new Clase(dto.getId(), dto.getTipo(), dto.getHorario(), dto.getCapacidadMaxima(),id.getId);
+    }
+
+}
--- src/Modelo/Pago.java
+++ src/Modelo/Pago.java
@@ -18,6 +18,63 @@ public class Pago {
     private double impuesto;
     private double total;
 
+    public int getId() {
+        return id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public double getImpuesto() {
+        return impuesto;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public void setImpuesto(double impuesto) {
+        this.impuesto = impuesto;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Pago(int id, int idCliente, Date fecha, double subtotal, double impuesto, double total) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.fecha = fecha;
+        this.subtotal = subtotal;
+        this.impuesto = impuesto;
+        this.total = total;
+    }
+    
     public void calcularTotales() {
         impuesto = subtotal * 0.13;
         total = subtotal + impuesto;
